945571b8cfe7aa8d7d9f8c07f1ffd88aef560f3f
Aryan1982 <ap1982o3@gmail.com>
2022-07-24 17:19:08 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Home from './components/Home'
+import About from './components/About'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div >
+      
+        <Routes>
+          <Route path="/" element={<Home/>}>
+          </Route>
+          <Route path="/about" element={<About/>}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+     );
+}
+
+export default App;
