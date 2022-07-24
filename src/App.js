23e44b78c4c550991045e681ed4b5e01027d1617
Aryan1982 <ap1982o3@gmail.com>
2022-07-24 17:57:14 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import Home from './components/Home'
 import About from './components/About'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {HashRouter as Router,Routes,Route} from "react-router-dom";
 
 
 function App() {
