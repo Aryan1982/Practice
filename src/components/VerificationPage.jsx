b5d7e9cbdd6a96396f830b038ef54d2571806880
Aryan1982 <ap1982o3@gmail.com>
2022-12-11 09:51:42 +0530

    done

--- src/components/VerificationPage.jsx
+++ src/components/VerificationPage.jsx
@@ -8,6 +8,9 @@ const VerificationPage = () => {
         <h1>Enter Verification Code</h1>
         <h2>Your verification code has been
             sent to your email address.</h2>
+        
+        <input id="partitioned" type="text" maxlength="4" />
+
         <button className="verificationBtn">VERIFY</button>
       <h3>Didn't recieve the OTP code?<br/> Resend OTP</h3>
       </div>
